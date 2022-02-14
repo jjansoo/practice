b736d6d1e2aa1964c70c2d5db9eaed3a355db58c
JinS <dkdlfjs7894@naver.com>
2022-02-14 23:45:10 +0900

    for 문을 이용한 구구단,, 위에꺼는 2~9단을 뽑아내는것이고,,
    아래는 내가 보고싶은단을 볼수잇ㄷ다,,,,

---
+++ backjoon 2739.py
@@ -0,0 +1,12 @@
+for i in range(2,10): # 1단 ~ 9단
+    for j in range(1,10): # x 1~9
+        print(f'{i}*{j}={i*j}')
+
+
+print('*'*40)
+
+
+
+num = int(input())
+for i in range(1,10):
+    print(num,'*',i,'=',num*i)
